44df2567368b1d12314a3433cc4f65ce0931f959
Quang Hung <hungpham307@gmail.com>
2023-07-13 16:44:27 +0700

    feat: implement create employee form

---
+++ app/admin/employees/new-employee/page.tsx
@@ -0,0 +1,5 @@
+import EmployeeForm from "@/components/employee-form";
+
+export default function AddEmployeeForm() {
+  return <EmployeeForm title="Create new employee" />;
+}
---
+++ components/employee-form.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { useToast } from "./ui/use-toast";
+
+const FormSchema = z.object({
+  userName: z
+    .string()
+    .min(3, {
+      message: "Username must be at least 3 characters long",
+    })
+    .max(20),
+  firstName: z.string().min(1, {
+    message: "First name is required",
+  }),
+  lastName: z.string().min(1, {
+    message: "Last name is required",
+  }),
+  city: z.string().min(1, {
+    message: "City is required",
+  }),
+});
+
+export default function EmployeeForm({ title }: { title: string }) {
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      userName: "",
+      firstName: "",
+      lastName: "",
+      city: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof FormSchema>) => {
+    try {
+      const response = await axios.post(
+        "https://localhost:7133/api/v1/User/CreateUser",
+        {
+          Username: data.userName,
+          FirstName: data.firstName,
+          LastName: data.lastName,
+          City: data.city,
+        }
+      );
+
+      if (response.status === 200) {
+        toast({
+          title: "New employee created",
+          description: `Welcome, ${data.firstName} ${data.lastName}!`,
+        });
+        // redirect to product list
+        router.push("/products");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <section>
+      <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+        <h1 className="text-3xl font-semibold mb-8">{title}</h1>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+              <div className="sm:col-span-2">
+                <FormField
+                  control={form.control}
+                  name="userName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div>
+                <FormField
+                  control={form.control}
+                  name="firstName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>First Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div>
+                <FormField
+                  control={form.control}
+                  name="lastName"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Last Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div className="sm:col-span-2">
+                <FormField
+                  control={form.control}
+                  name="city"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>City</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="inline-flex items-center px-5 py-2.5 mt-8 text-sm font-medium text-center text-white bg-gray-700 rounded-md"
+            >
+              Create
+            </button>
+          </form>
+        </Form>
+      </div>
+    </section>
+  );
+}
